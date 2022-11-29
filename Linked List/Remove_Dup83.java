dfe7488ee2d232204336f6d11e7e182551e5da8d
Hiren Timbadiya <hirentimbadiya74@gmail.com>
2022-11-29 11:17:42 +0530

    Added removeDup83.java

---
+++ Linked List/Remove_Dup83.java
@@ -0,0 +1,36 @@
+public class Remove_Dup83 {
+    static ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        if (head == null)
+            return head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
