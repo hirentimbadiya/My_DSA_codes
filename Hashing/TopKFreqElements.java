32e713b51ee29090dd4eb22c6c41f9264b90d27b
Hiren Timbadiya <hirentimbadiya74@gmail.com>
2022-12-08 09:21:30 +0530

    created TopKFreqElements.java

---
+++ Hashing/TopKFreqElements.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TopKFreqElements {
+    static int[] topKFrequent(int[] nums, int k) {
+        int[] ans = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        int i = 0;
+        while (i < k) {
+            int max = findMax(map, nums);
+            ans[i] = max;
+            map.remove(max);
+            i++;
+        }
+        return ans;
+    }
+
+    static int findMax(HashMap<Integer, Integer> map, int[] nums) {
+        int val = -1;
+        int max = 0;
+        // iterate over the hashmap , its a important syntax.
+        for (Map.Entry<Integer, Integer> key : map.entrySet()) {
+            if (max < key.getValue()) {
+                val = key.getKey();
+                max = key.getValue();
+            }
+        }
+
+        return val;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 1, 2, 2, 3 };
+        System.out.println(Arrays.toString(topKFrequent(arr, 2)));
+    }
+}
