36ae9cf050cb44cc9ad871acd08715e94836fcc4
Hiren Timbadiya <hirentimbadiya74@gmail.com>
2022-12-15 20:49:30 +0530

    updated it

--- Sorting-Algorithms/BubbleSort.java
+++ Sorting-Algorithms/BubbleSort.java
@@ -1,21 +1,18 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int size = 50000;
-        int[] A = generateArray(size);
-        long start = System.currentTimeMillis();
+        int[] A = { 1, 2, 5, 67, 8, 0, 9, 6 };
         Sort(A);
-        long end = System.currentTimeMillis();
-        long time = (end - start);
-        System.out.println("time : " + time);
+        System.out.println(Arrays.toString(A));
     }
 
     static void Sort(int[] arr) {
         boolean swapped;
-        for (int i = 0; i < arr.length; i++) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
             swapped = false;
-            for (int j = 1; j < arr.length - i; j++) {
+            for (int j = 1; j < n - i; j++) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
@@ -27,14 +24,5 @@ public class BubbleSort {
                 break;
             }
         }
-    }
-
-    static int[] generateArray(int size) {
-        Random r = new Random();
-        int[] arr = new int[size];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = r.nextInt();
-        }
-        return arr;
     }
 }
